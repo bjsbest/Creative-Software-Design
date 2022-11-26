a49721f021d335b8b45171b2bce80b5b509d3b3b
bjsbest <bjsbest@hanyang.ac.kr>
2022-11-27 02:49:00 +0900

    Assignment 12-2

--- 12-2/1/list.h
+++ 12-2/1/list.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <iostream>
+using namespace std;
+
 template <class T>
 class Node{
     public:
@@ -41,13 +44,38 @@ class List{
     }
 
     void insert_at(int idx, const T& data){
-        // 기존에 있던 list의 idx 값을 뒤로 밀어내고, 그 자리를 새로운 node가 채운다.
-        // 새로운 node를 "마지막에" 붙여놓고, idx부터 값을 하나씩 뒤로 미룬다.
+        Node<T>* new_n = new Node<T>();
+        new_n->data = data;
+
+        Node<T>* n = head;
+        int cnt = 0;
+        while(cnt < idx){
+            Node<T>* temp = n;
+            n = n->next;
+        }
+    }
+
+    void remove_at(int idx){
+        // n번째 indext의 node를 remove하면 n-1번째의 next를 기존 n+1번째의 node로 옮긴다.
+        Node<T>* n = head;
+    }
+
+    void pop_back(){
+        Node<T>* n = head;
+        while(n->next != NULL){
+            Node<T>* temp = n->next;
+            n = temp;
+        }
+        // while 탈출 -> n = NULL
+        delete n;
+    }
+
+    void push_back(const T& val){
+        // 마지막 위치에 노드 붙이기
         Node<T>* new_n = new Node<T>();
         new_n->data = data;
         new_n->next = NULL;
 
-        /* 마지막 위치에 노드 붙이기
         Node<T>* n = head;
         while(true){
             if(n != NULL){
@@ -58,37 +86,21 @@ class List{
                 n->next = new_n;
                 break;
             }
-        } */
-        
-        // "원하는" 위치에 노드 붙이기
-        Node<T>* new_n = new Node<T>();
-        new_n-> data = data;
-        new_n->next = NULL;
-
-        for(int cnt = 0; i<idx + 1; cnt++){
-            
         }
-
-    }
-
-    void remove_at(int idx){
-
-    }
-
-    void pop_back(){
-
-    }
-
-    void push_back(const T& val){
-
     }
 
     void pop_front(){
-
+        Node<T>* n = head;
+        head = n->next;
+        delete n;
     }
 
     void push_front(const T& val){
-
+        Node<T>* new_n = new Node<T>();
+        new_n->data = val;
+        Node<T>* temp = head;
+        new_n->next = temp;
+        head = new_n;
     }
 
     friend ostream& operator<<(ostream& out, List& rhs){ // print out nodes.
